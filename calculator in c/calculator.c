8b6700b74205fdf9e9eca50de6e146c8b421e8c9
Raj shekar <93192586+Raja234raj@users.noreply.github.com>
2023-12-10 18:03:09 +0530

    Add files via upload

---
+++ calculator in c/calculator.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<conio.h>
+void addition(int num1, int num2){
+    int result = num1 + num2;
+    printf("%d", result);
+}
+void subtraction(int num1, int num2){
+    int result = num1 - num2;
+    printf("%d", result);
+}
+void multiply(int num1, int num2){
+    int result = num1 * num2;
+    printf("%d", result);
+}
+void division(int num1, int num2){
+    if(num2 != 0){
+        float result = (float)num1 / num2;
+        printf("%.2f\n", result);
+
+    }else{
+        printf("Error! division with Zero!!");
+    }
+}
+int main(){
+    int num1, num2;
+    printf("Enter the frist number: ");
+    scanf("%d", &num1);
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    char operator;
+    printf("Choose a operator(+, -, *, /)");
+    scanf("%c", &operator);
+
+    if(operator == '+'){
+        addition(num1, num2);
+    } else if(operator == '-'){
+        subtraction(num1, num2);
+
+    } else if (operator == '*'){
+        multiply(num1, num2);
+
+    } else if(operator == '/'){
+        division(num1, num2);
+    } else{
+        printf("Invalid input !!");
+
+    }
+
+    return 0;
+}
